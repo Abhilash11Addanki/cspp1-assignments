aa80d6adc46be1c1c2f794f96f4e39914340f363
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-02 19:18:04 +0530

    " -> 5 of 5 passed. pylint: 6.88/10 "

--- Module 4/p3/longest_substring.py
+++ Module 4/p3/longest_substring.py
@@ -1,18 +1,18 @@
 '''Assume s is a string of lower case characters.'''
 def main():
-    s = input()
+    str_1 = input()
     max_str = ""
-    for j in range(len(s)):
+    for j in range(len(str_1)):
         sub_str = ""
-        t = s[j]
-        for i in range(j, len(s)):
-            if t <= s[i]:
-                t = s[i]
+        t = str_1[j]
+        for i in range(j, len(str_1)):
+            if t <= str_1[i]:
+                t = str_1[i]
                 sub_str += t
             else:
                 break
         if len(max_str) < len(sub_str):
             max_str = sub_str         
     print(max_str)
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
