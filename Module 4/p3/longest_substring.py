2b64af5c2ee6a59096df38a530c98c99b2b43dba
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-02 19:05:04 +0530

    " -> 5 of 5 passed. pylint: 3.85/10 "

--- Module 4/p3/longest_substring.py
+++ Module 4/p3/longest_substring.py
@@ -1,16 +1,16 @@
-s=input()
-max_str=""
+s = input()
+max_str = ""
 for j in range(len(s)):
-    sub_str=""
-    t=s[j]
+    sub_str = ""
+    t = s[j]
     for i in range(j,len(s)):
-        if t<=s[i]:
-            t=s[i]
-            sub_str+=t
+        if t <= s[i]:
+            t = s[i]
+            sub_str += t
         else:
             break
     if len(max_str) < len(sub_str):
-        max_str=sub_str
+        max_str = sub_str
             
 print(max_str)
 
