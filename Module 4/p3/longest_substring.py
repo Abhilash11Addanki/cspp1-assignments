51ac5c4a8fe6d60d0d726a4cf882628312926f15
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-02 19:15:06 +0530

    " -> 5 of 5 passed. pylint: 5.00/10 "

--- Module 4/p3/longest_substring.py
+++ Module 4/p3/longest_substring.py
@@ -1,19 +1,18 @@
-s = input()
-max_str = ""
-for j in range(len(s)):
-    sub_str = ""
-    t = s[j]
-    for i in range(j,len(s)):
-        if t <= s[i]:
-            t = s[i]
-            sub_str += t
-        else:
-            break
-    if len(max_str) < len(sub_str):
-        max_str = sub_str
-            
-print(max_str)
-
-
-        
-        
+'''Assume s is a string of lower case characters.'''
+def main():
+    s = input()
+    max_str = ""
+    for j in range(len(s)):
+        sub_str = ""
+        t = s[j]
+        for i in range(j, len(s)):
+            if t <= s[i]:
+                t = s[i]
+                sub_str += t
+            else:
+                break
+        if len(max_str) < len(sub_str):
+            max_str = sub_str         
+    print(max_str)
+if __name__== "__main__":
+	main()
