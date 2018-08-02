90cc9383fedeaf12d3cc6196004dcc6a9d56840e
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-02 14:57:04 +0530

    " -> 5 of 5 passed. pylint: 7.00/10 "

--- Module 4/p2/bob_counter.py
+++ Module 4/p2/bob_counter.py
@@ -5,15 +5,15 @@ Write a program that prints the number of times the string 'bob' occurs in s. Fo
 Number of times bob occurs is: 2'''
 
 def main():
-    s = input()
-    s1='bob'
-    len1=len(s1)
-    cnt=0
-    for i in range(len(s)-2):
-        if s[i]+s[i+1]+s[i+2]==s1:
-            cnt=cnt+1
+    '''Main function'''
+    str_1 = input()
+    str_2 = 'bob'
+    cnt = 0
+    for i in range(len(str_1)-2):
+        if str_1[i]+str_1[i+1]+str_1[i+2] == str_2:
+            cnt = cnt+1
     print(cnt)
 	
 
-if __name__== "__main__":
-	main()
+if __name__ ==  "__main__":
+    main()
