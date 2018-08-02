a9cb9936d3cf6ac638125a6264aeaab01f52e3db
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-02 14:32:11 +0530

    " -> 4 of 4 passed. pylint: 4.44/10 "

--- Module 4/p1/vowels_counter.py
+++ Module 4/p1/vowels_counter.py
@@ -1,11 +1,11 @@
 """This program evaluates the number of vowels in a string"""
 def main():
-        S = input()
-        num = 0
-        for letter in s:
-            if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
-                num = num + 1
-        print(num)
+    S = input()
+    num = 0
+    for letter in S:
+        if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+            num = num+1
+    print(num)
 
 if __name__== "__main__":
 	main()
