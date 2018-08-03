96f71076a8b33002fbd915fe5a35115ea083c506
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-03 18:31:13 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- Module 5/p2/square_root.py
+++ Module 5/p2/square_root.py
@@ -8,7 +8,7 @@ def main():
     while abs(guess**2-num_1) >= epsilon:
         guess += step
     if abs(guess**2-num_1) >= epsilon:
-        print("Failed on square root of", s)
+        print("Failed on square root of", num_1)
     else:
         print(guess)
 if __name__ == "__main__":
