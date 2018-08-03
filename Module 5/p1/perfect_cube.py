fa334e5fd1c61f19609226fae75f50369815e458
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-03 18:39:18 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- Module 5/p1/perfect_cube.py
+++ Module 5/p1/perfect_cube.py
@@ -1,13 +1,13 @@
 '''This program evaluates whether given number is a perfect cube or not'''
 def main():
     '''Main function.'''
-    s = int(input())
+    num_1 = int(input())
     guess = 1
-    while guess**3 < s:
+    while guess**3 < num_1:
         guess += 1
-    if guess**3 == s:
-        print(s, "is a perfect cube")
+    if guess**3 == num_1:
+        print(num_1, "is a perfect cube")
     else:
-        print(s, "is not a perfect cube")
+        print(num_1, "is not a perfect cube")
 if __name__ == "__main__":
     main()
