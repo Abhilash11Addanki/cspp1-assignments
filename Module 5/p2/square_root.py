112d189272dce48abced2550680b01a0be1b7d69
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-03 18:26:19 +0530

    " -> 3 of 3 passed. pylint: 1.67/10 "

--- Module 5/p2/square_root.py
+++ Module 5/p2/square_root.py
@@ -1,15 +1,24 @@
-# Write a python program to find the square root of the given number 
+# Write a python program to find the square root of the given number
 # using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+
 def main():
-    s=int(input())
-    epsilon=0.001
-    guess=1
-    increment=0.0001
+    s = int(input())
+    epsilon = 0.01
+    step = 0.1
+    guess=0
     while abs(guess**2-s)>=epsilon:
-        guess+=increment
+        guess+=step
     if abs(guess**2-s)>=epsilon:
         print("Failed on square root of",s)
     else:
-        print(guess,"is close to the square root of",s)
+        print(guess)
 if __name__== "__main__":
-    main()
+	main()
