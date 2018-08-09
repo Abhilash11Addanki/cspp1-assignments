c74cfca074495aea29e5fd6cd3f71cd630a9758b
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-09 12:50:27 +0530

    how_many

---
+++ Module 10/how_many.py
@@ -0,0 +1,27 @@
+#Exercise : how many
+# write a procedure, called how_many, which returns the sum of the number of values associated with a dictionary.
+
+
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    list_1=[]
+    for i in aDict.keys():
+        list_1+=aDict[i]
+    return len(list_1)
+def main():
+    n=input()
+    aDict={}
+    for i in range(int(n)):
+        s=input()
+        l=s.split()
+        if l[0][0] not in aDict:
+            aDict[l[0][0]]=[l[1]]
+        else:
+            aDict[l[0][0]].append(l[1])
+    print(how_many(aDict))
+if __name__== "__main__":
+    main()
