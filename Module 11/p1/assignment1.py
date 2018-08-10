07385dc0752d0b5c329276d28d1ca3b3b5f90a24
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-10 20:24:37 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- Module 11/p1/assignment1.py
+++ Module 11/p1/assignment1.py
@@ -1,9 +1,8 @@
 '''
 Exercise: Assignment-1
-The first step is to implement some code that allows us to calculate the score for a single word. The function get_word_score should accept as input a string of lowercase letters (a word) and return the integer score for that word, using the game's scoring rules.
 '''
 
-def get_word_score(word, n):
+def get_word_score(word, n_num):
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -12,28 +11,27 @@ def get_word_score(word, n):
     letters are used on the first turn.
 
     Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
-    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+    worth 3, D is worth 2, E is worth 1, and so on (see secret_letters_values)
 
     word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
     # TO DO ... <-- Remove this comment when you code this function
-    score = 0
-    SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
-    l = len(word)
+    score_num = 0
+    secret_letters_values = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4,
+                             'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3,
+                             'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
+                             'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    l_num = len(word)
     for i in word:
-        if i in SCRABBLE_LETTER_VALUES.keys():
-            score += SCRABBLE_LETTER_VALUES[i]
-    score *= l
-    if l == n:
-        score += 50
-        return score
-    else:
-        return score
+        if i in secret_letters_values.keys():
+            score_num += secret_letters_values[i]
+    score_num *= l_num
+    if l_num == n_num:
+        score_num += 50
+        return score_num
+    return score_num
 def main():
     '''
     Main function for the given problem
