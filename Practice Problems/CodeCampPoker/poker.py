a7ff730bea846a51f3a59031932a9ad5a5dc2be7
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-15 23:03:03 +0530

    " -> 5 of 8 passed. pylint: 9.41/10 "

--- Practice Problems/CodeCampPoker/poker.py
+++ Practice Problems/CodeCampPoker/poker.py
@@ -38,15 +38,28 @@ def is_flush(hand):
     if cnt1 == 1:
         return True
     return False
+def is_threeofakind(hand):
+    '''
+    Function for finding three of a kind
+    '''
+    list_1=[]
+    for i in hand[0]:
+        list_1.append(i[0])
+    for i in hand[0]:
+        if list_1.count(i[0])==3:
+            return True
+        return False
 def hand_rank(hand):
     '''
     Function for finding the rank of a hand
     '''
     if is_straight(hand) and is_flush(hand):
-        return 3
+        return 4
     if is_flush(hand):
-        return 2
+        return 3
     if is_straight(hand):
+        return 2
+    if is_threeofakind(hand):
         return 1
     return 0
 
