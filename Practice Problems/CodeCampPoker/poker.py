3269b89c02fa40ab5de2648f1e8f5fd338d24368
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-16 00:45:54 +0530

    " -> 7 of 8 passed. pylint: 7.96/10 "

--- Practice Problems/CodeCampPoker/poker.py
+++ Practice Problems/CodeCampPoker/poker.py
@@ -88,21 +88,14 @@ def is_fullhouse(hand):
 def is_twopair(hand):
     '''Function for finding two pair'''
     cnt = 0
-    cnt1 = 0
     list_1=[]
-    list_2=[]
     for i in hand:
         list_1.append(i[0])
     set_1 = set(list_1)
     for i in list_1:
         if list_1.count(i)==2:
-            cnt = 1
-            list_2 = list_1-[i]
-            break
-    for i in list_2:
-        if list_2.count(i)==2:
-            cnt1 = 1
-    if cnt==1 and cnt1==1 and len(set_1)==3:
+            cnt += 1
+    if cnt==4 and len(set_1)==4:
         return True
     return False    
 def is_onepair(hand):
