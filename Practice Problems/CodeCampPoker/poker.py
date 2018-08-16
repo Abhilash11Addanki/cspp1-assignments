480489313ac0aee2201ebb2d5ebde60a9c1eca95
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-16 12:13:57 +0530

    " -> 17 of 20 passed. pylint: 8.51/10 "

--- Practice Problems/CodeCampPoker/poker.py
+++ Practice Problems/CodeCampPoker/poker.py
@@ -57,10 +57,10 @@ def is_fourofakind(hand):
     for i in hand:
         list_1.append(i[0])
     set_1 = set(list_1)
-    for i in list_1:
+    for i in set_1:
         if list_1.count(i)==4:
-            cnt += 1
-    if cnt == 4 and len(set_1) == 2:
+            cnt = 1
+    if cnt == 1 and len(set_1) == 2:
         return True
     return False
 def is_fullhouse(hand):
@@ -71,12 +71,12 @@ def is_fullhouse(hand):
     for i in hand:
         list_1.append(i[0])
     set_1 = set(list_1)
-    for i in list_1:
+    for i in set_1:
         if list_1.count(i)==3:
-            cnt += 1
+            cnt = 1
         if list_1.count(i)==2:
-            cnt1 += 1
-    if (cnt==3 and cnt1==2) and len(set_1)==2:
+            cnt1 = 1
+    if (cnt==1 and cnt1==1) and len(set_1)==2:
         return True
     return False
 def is_twopair(hand):
