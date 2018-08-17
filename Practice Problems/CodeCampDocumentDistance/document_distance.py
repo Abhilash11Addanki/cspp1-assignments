45b73fa33d17520aa3cb4af61d03f97295597f89
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-17 12:41:48 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- Practice Problems/CodeCampDocumentDistance/document_distance.py
+++ Practice Problems/CodeCampDocumentDistance/document_distance.py
@@ -1,9 +1,11 @@
-'''Document Distance - A detailed description is given in the PDF'''
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
 import math
 def similarity(dict1, dict2):
-    '''Compute the document distance as given in the PDF'''
-    dict1 = dict1.lower()
-    dict2 = dict2.lower()
+    '''
+        Compute the document distance as given in the PDF
+    '''
     dict_1 = {}
     dict_2 = {}
     word_freq = {}
@@ -39,16 +41,22 @@ def similarity(dict1, dict2):
     denom_n = d_1*d_2
     return round(numer_n/denom_n, 1)
 def load_stopwords(filename):
-    '''loads stop words from a file and returns a dictionary'''
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
     stopwords = {}
     with open(filename, 'r') as filename:
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
+
 def main():
-    '''take two inputs and call the similarity function'''
+    '''
+        take two inputs and call the similarity function
+    '''
     input1 = input()
     input2 = input()
+
     print(similarity(input1, input2)
 
 if __name__ == '__main__':
