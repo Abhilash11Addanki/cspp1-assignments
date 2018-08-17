b9c5f0e83654e4a937fb45e5d7ea41b25348d06a
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-17 11:57:58 +0530

    " -> 0 of 3 passed. pylint: 8.25/10 "

--- Practice Problems/CodeCampDocumentDistance/document_distance.py
+++ Practice Problems/CodeCampDocumentDistance/document_distance.py
@@ -50,7 +50,7 @@ def similarity(dict1, dict2):
         denom_n2 += word_freq[i][1]**2
     d_2 = math.sqrt(denom_n2)
     denom_n = d_1*d_2
-    return (numer_n/denom_n)
+    return (round(numer_n/denom_n))
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
