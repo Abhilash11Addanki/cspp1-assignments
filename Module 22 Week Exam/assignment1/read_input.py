9d47271075e9d91a6f27e9ca9cdaa89394f90f91
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-25 10:30:43 +0530

    " -> 0 of 2 passed. pylint: 7.78/10 "

---
+++ Module 22 Week Exam/assignment1/read_input.py
@@ -0,0 +1,14 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    n_1 = input()
+    res_str = ""
+    for i in range(n_1):
+        read_input = input()
+        res_str += read_input
+    print(res_str)
+
+if __name__ == '__main__':
+    main()
--- Practice Problems/Code Camp-Tic Tac Toe/tic_tac_toe.py
+++ Practice Problems/Code Camp-Tic Tac Toe/tic_tac_toe.py
@@ -1,9 +1,54 @@
+def rows(game_hand):
+    winner_x = False
+    winner_o = False
+    for row in game_hand:
+        if row.count('x') == 3:
+            winner_x = True
+        if row_count('o') == 3:
+            winner_o = True
+    if winner_x and winner_o:
+        print("invalid game")
+        exit()
+    if winner_x:
+        return (True, "x")
+    if winner_o:
+        return (True, "o")
+    return (False, 0)
+def column(game_hand):
+    transpose = []
+    for i in range(len(game_hand)):
+        row = []
+        for j in range(len(game_hand[0])):
+            row.append(game_hand[j][i])
+        transpose.append(row)
+    return rows(transpose)
+def diagnols(game_hand):
+    d1 =[]
+    for i in range(len(game_hand)):
+	d1.append(game_hand[i][i])
+    if d1.count("o")==3:
+	return (True,"o")
+    if  d1.count("x")==3:
+	return (True,"x")
+    d2 =[]
+    j=len(game_hand[0])-1
+    for i in range(len(game_hand)):
+	d2.append(game_hand[i][j])
+	j=j-1
+    if d2.count("o")==3:
+	return (True,"o")
+    if  d2.count("x")==3:
+	return (True,"x")
+    return (False,0)
 def win_hand(game_hand):
-    for i in game_hand:
-        if i.count('x') == 3:
-            return 'x'
-        if i.count('o') == 3:
-            return 'o'
+    if rows(game_hand)[0]:
+        print(rows(game_hand)[1])
+    elif column(game_hand)[0]:
+        print(column(game_hand)[1])
+    elif diagnols(game_hand)[0]:
+        print(diagnols(game_hand)[1])
+    else:
+        print("draw")
 def invalid_input(game_hand):
     for i in game_hand:
         if i[0] not in 'xo.' or i[1] not in 'xo.' or i[2] not in 'xo.':
@@ -14,7 +59,7 @@ def invalid_game(game_hand):
     for i in game_hand:
         cnt_x += i.count('x')
         cnt_o += i.count('o')
-    return True if cnt_x-cnt_o > 1 or cnt_o-cnt_x > 1 else False
+    return True if abs(cnt_x-cnt_o) == 1 or abs(cnt_x-cnt_o) == 0 else False
 def play_game(game_hand):
     '''Function for playing the game'''
     if invalid_input(game_hand) is True:
