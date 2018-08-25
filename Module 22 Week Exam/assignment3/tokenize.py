21e0b5c97d95eec6855e670a20da0b61b8f8d177
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-25 11:23:53 +0530

    " -> 3 of 4 passed. pylint: 7.69/10 "

--- Module 22 Week Exam/assignment3/tokenize.py
+++ Module 22 Week Exam/assignment3/tokenize.py
@@ -2,7 +2,6 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
 def tokenize(string):
     '''tokenize function.'''
     dict_ = {}
@@ -10,13 +9,11 @@ def tokenize(string):
     for char in list_:
     	dict_[char] = list_.count(char)
     return dict_
-            
 def main():
     '''main function.'''
     n_1 = int(input())
     for str_1 in range(n_1):
     	str_1 = input()
     print(tokenize(str_1))
-
 if __name__ == '__main__':
     main()
