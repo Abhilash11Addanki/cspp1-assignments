d5a94b461429bd82c3a3391c4c8a145c578f00b5
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-25 11:50:50 +0530

    " -> 3 of 4 passed. pylint: 9.23/10 "

--- Module 22 Week Exam/assignment3/tokenize.py
+++ Module 22 Week Exam/assignment3/tokenize.py
@@ -5,20 +5,15 @@ each word
 def tokenize(string):
     '''tokenize function.'''
     dict_ = {}
-    list_1 = []
-    list_ = string.split(";")
-    for i in list_:
-        list_1.append(i.split(" "))
-    for char in list_1:
-        dict_[char] = list_1.count(char)
+    list_ = string.split(" ")
+    for char in list_:
+        dict_[char] = list_.count(char)
     return dict_
 def main():
     '''main function.'''
     n_1 = int(input())
-    res_str = ""
     for str_1 in range(n_1):
         str_1 = input()
-        res_str += str_1
-    print(tokenize(res_str))
+    print(tokenize(str_1))
 if __name__ == '__main__':
     main()
