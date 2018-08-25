29274038a45ea2b6a4abdd4503c2724e8db27ead
Abhilash11Addanki <abhilash11addanki@msitprogram.net>
2018-08-25 10:41:45 +0530

    " -> 2 of 3 passed. pylint: 9.17/10 "

---
+++ Module 22 Week Exam/assignment2/clean_input.py
@@ -0,0 +1,21 @@
+'''
+Write a function to clean up a given string by removing the special characters and retain 
+alphabets in both upper and lower case and numbers.
+'''
+
+def clean_string(string):
+    '''function to clean string'''
+    res_str = ""
+    for char in string:
+        if char in "!@#$%^&*()_+":
+            res_str += ""
+        else:
+            res_str += char
+    return res_str
+def main():
+    '''main function'''
+    string = input()
+    print(clean_string(string))
+
+if __name__ == '__main__':
+    main()
